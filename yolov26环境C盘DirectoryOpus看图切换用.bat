391463dcf5e7f7a5f24cf978620e739e41144fdf
lhj5426 <30548000+lhj5426@users.noreply.github.com>
2026-06-21 04:23:54 +0800

    260621 为Directory Opus 添加文件夹传递专用BAT
    260621 为Directory Opus 添加文件夹传递专用BAT

---
+++ yolov26环境C盘DirectoryOpus看图切换用.bat
@@ -0,0 +1,10 @@
+@echo off
+cd /d "%~dp0"
+
+if not "%~1"=="" (
+    C:\ProgramData\miniconda3\pythonw.exe anylabeling/fast_send.py "%~1"
+    if not errorlevel 1 exit /b 0
+)
+
+call conda activate yolov26
+python anylabeling/app.py %*
