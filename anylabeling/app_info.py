76333fc5e3fd34127a92001ea765187c412866ca
lhj5426 <30548000+lhj5426@users.noreply.github.com>
2026-07-27 11:31:52 +0800

    Add files via upload

--- anylabeling/app_info.py
+++ anylabeling/app_info.py
@@ -1,5 +1,5 @@
 ﻿# -*- coding: utf-8 -*-
-__appname__ = "Hmogai_260725_X-AnyLabeling变色标签"
+__appname__ = "Hmogai_X-AnyLabeling"
 __appdescription__ = "Advanced Auto Labeling Solution with Added Features"
 __version__ = "3.2.2"
 __preferred_device__ = "GPU"  # GPU or CPU
