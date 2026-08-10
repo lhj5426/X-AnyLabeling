790a803102ff1753f156a5cc2fec43ba5235c340
lhj5426 <30548000+lhj5426@users.noreply.github.com>
2026-08-10 14:49:05 +0800

    Add files via upload

--- anylabeling/app_info.py
+++ anylabeling/app_info.py
@@ -1,6 +1,6 @@
 ﻿# -*- coding: utf-8 -*-
 __appname__ = "YSG-AnyLabeling"
 __appdescription__ = "Advanced Auto Labeling Solution with Added Features"
-__version__ = "9.0.0"
+__version__ = "1.0.0"
 __preferred_device__ = "GPU"  # GPU or CPU
 __url__ = "https://t.me/yinshuguan"
